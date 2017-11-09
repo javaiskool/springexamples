6d785ad2222d7c4ebcdc4a89dd7c3f9dd8201225
Vijay Kumar <futureinjava@gmail.com>
2017-11-09 22:00:34 +0530

    spring boot example with hello css

---
+++ src/main/java/com/dt/RddemoApplication.java
@@ -0,0 +1,12 @@
+package com.dt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RddemoApplication.class, args);
+	}
+}
